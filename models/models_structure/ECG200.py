b9912a4689ce256bb71fc878eb2ffc396219d114
FlaAI <58940661+FlaAI@users.noreply.github.com>
2023-02-09 21:56:55 +0800

    Add files via upload

---
+++ models/models_structure/ECG200.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+        self.rnn = nn.LSTM(
+            input_size=1,
+            hidden_size=128,
+            num_layers=2,
+            batch_first=True
+        )
+        self.out = nn.Linear(128, 2)
+
+    def forward(self, x):
+        r_out, (h_n, h_c) = self.rnn(x, None)  # None represents zero initial hidden state
+        out = self.out(r_out[:, -1, :])        # choose r_out at the last time step
+        out_trace = self.out(r_out)            # choose r_out at all time steps
+        return out, out_trace
